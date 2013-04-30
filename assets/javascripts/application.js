11bec374c768c9083cce33d545f6a60d94897625
Sean Connelly <sean@seanconnelly.me>
2013-04-29 22:07:54 -0700

    Adding tabs and cleaning out unnecessary migrated Javascript

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,3 +1,14 @@
 /*jslint browser: true*/
 /*global $, jQuery, moment*/
 // This is a manifest file that'll be compiled into application.min.js
+
+$(document).ready(function(){
+	//Setup the default bootstrap tabs
+	$('#myTab a').click(function (e) {
+  		e.preventDefault();
+  		$(this).tab('show');
+	})
+
+	//Start with the first tab active
+	$('#myTab a:first').tab('show');
+});
--- index.php
+++ index.php
@@ -3,19 +3,22 @@ include 'views/header.php';
 include 'views/topnav.php';
 require_once 'models/games.php';
 ?>
-
 <div class="container">
   <div class="span12">
-    <ul class="nav nav-tabs">
+    <h4>Choose from a feed</h4>
+    <ul class="nav nav-tabs" id="myTab">
       <li><a href="#games" data-toggle="tab">Games</a></li>
       <li><a href="#recent-results" data-toggle="tab">Recent Results</a></li>
     </ul>
-    <div id="games">
-      <?php include 'views/games.php'; ?>
+    <div class="tab-content">
+      <div class="tab-pane active" id="games">
+        <?php include 'views/games.php'; ?>
+      </div>
+     <div class="tab-pane" id="recent-results">
+        <?php include 'views/recent-results.php'; ?>
+      </div>
     </div>
-    <div id="recent-results">
-      <?php include 'views/recent-results.php'; ?>
-    </div>
+
   </div> <!-- .span12 -->
 </div> <!-- .container -->
 <?php include 'views/footer.php'; ?>
