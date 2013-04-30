430c3572065785d12620035c45535c7914258f7d
Sean Connelly <sean@seanconnelly.me>
2013-04-29 22:33:02 -0700

    Updating the footer, removing social networks

--- views/footer.php
+++ views/footer.php
@@ -10,30 +10,10 @@
 								<li><a href="http://app.rsportz.com/privacy">Privacy</a></li>
 							</ul>
 						</div>
-						<div class='span4 share-container'>
-							<div class='share-links'>
-								<div class='facebook-share'>
-									<div class='fb-like' data-href='http://www.facebook.com/rsportz' data-layout='button_count' data-send='false' data-width='90'></div>
-									<fb-root></fb-root>
-									<script>
-	  								//<![CDATA[
-	    								(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = "//connect.facebook.net/en_US/all.js#xfbml=1"; fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
-	  								//]]>
-									</script>
-								</div>
-							<div class='twitter-share'>
-								<a href="https://twitter.com/rsportz" class="twitter-follow-button" data-show-count="none" data-url="http://www.rsportz.com">Tweet</a>
-								<script>
-	  							//<![CDATA[
-	    							!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-	  							//]]>
-								</script>
-							</div>
-						</div>
-					</div>
+						<div class='span4 centre'>RSportz 1.0 Feed Test</div>
 					<div class='span4'>
 						<p class='mobile-padding'>
-							&copy;2003 - 2013 SwooshDot, Inc. All rights reserved.
+							&copy;2003 - 2013 RSportz, Inc. All rights reserved.
 						</p>
 					</div>
 				</div>
