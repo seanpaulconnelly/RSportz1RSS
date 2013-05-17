087fea2776bab9ddec5e80919e9f85d72392c9f6
Sean Connelly <sean@seanconnelly.me>
2013-05-17 12:22:58 -0700

    Adding https:// to the feeds

--- models/games.php
+++ models/games.php
@@ -1,6 +1,6 @@
 <?php
 //Call the Games URL -- using Skate San Diego as a test subject
-$gamesFeed = simplexml_load_file("http://app.rsportz.com/entity_calendar/upcoming_games/47.rss");
+$gamesFeed = simplexml_load_file("https://app.rsportz.com/entity_calendar/upcoming_games/47.rss");
 //Set the 'item's as games
 $games = $gamesFeed->channel->item;
 ?>
--- models/recent-results.php
+++ models/recent-results.php
@@ -1,6 +1,6 @@
 <?php
 //Call the Recent Results URL -- using Skate San Diego as a test subject
-$resultsFeed = simplexml_load_file("http://app.rsportz.com/entity_calendar/recent_results/47.rss");
+$resultsFeed = simplexml_load_file("https://app.rsportz.com/entity_calendar/recent_results/47.rss");
 //Set the 'item's as games
 $results = $resultsFeed->channel->item;
 ?>
