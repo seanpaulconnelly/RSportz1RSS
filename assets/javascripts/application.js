e3f69edf83304cf9ddf9b6e1395a4863de9e8eb0
Sean Connelly <sean@seanconnelly.me>
2013-06-25 10:18:10 -0700

    Deconstructing assets

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -3,12 +3,5 @@
 // This is a manifest file that'll be compiled into application.min.js
 
 $(document).ready(function(){
-	//Setup the default bootstrap tabs
-	$('#myTab a').click(function (e) {
-  		e.preventDefault();
-  		$(this).tab('show');
-	})
-
-	//Start with the first tab active
-	$('#myTab a:first').tab('show');
+	//do stuff
 });
