dfec10b3949a3107f0241ad2f716a768fe31ebe8
Sean Connelly <sean@seanconnelly.me>
2013-06-25 22:15:46 -0700

    Sidebar and Twitter API

--- jos/sidebar.php
+++ jos/sidebar.php
@@ -16,11 +16,27 @@
 			<p class="lead"><?php echo $currentTime; ?></p>
 		</div>
 	</div>
-	<div class="row">
-		<div class="span4 outbound">
-			<hr>
-			<p class="lead">Full schedule at<br/>2013aauinlinejos.org</p>
-			<p class="lead">Twitter Feed</p>
+	<div class="row link">
+		<div class="span4">
+			<p>Visit</p>
+			<p class="lead">2013AAUInlineJOs.org</p>
+			<p>For Schedule, Stats, and Standings</p>
+		</div>
+	</div>
+	<div class="row twitter">
+		<div class="span4">
+	<?php include '../../Oauth.php';		
+	$bearer_token = get_bearer_token(); // get the bearer token
+	$json = json_decode(search_for_a_term($bearer_token, "%23stanleycup", 'mixed', '5'));
+	$tweetText = $json->statuses['0']->text;
+	$screenName = $json->statuses['0']->user->screen_name;
+	$profileImageUrl = $json->statuses['0']->user->profile_image_url_https;
+      echo $profileImageUrl;
+      echo $tweetText;
+      echo '<br/>@' . $screenName;
+      
+	invalidate_bearer_token($bearer_token); // invalidate the token
+	?>
 		</div>
 	</div>
 </section>
