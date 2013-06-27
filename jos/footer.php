a6040bc2d76d00b34c098615cd1dc68a6d130749
Sean Connelly <sean@seanconnelly.me>
2013-06-26 18:05:41 -0700

    Adding instagram icon to footer

--- jos/footer.php
+++ jos/footer.php
@@ -4,8 +4,8 @@
 				<div class="span6">
 					<p class="lead">Join in with Twitter or Instagram</p>
 				</div>
-				<div class="span5 offset1">
-					<p class="lead"><img src="../../assets/images/twitter-bird-small.png" alt="Source "> #aau2013jos</p>
+				<div class="span4 offset2">
+					<p class="lead"><img src="../../assets/images/instagram-icon-small.png" alt="Tag"> <img src="../../assets/images/twitter-bird-small.png" alt="Tweet"> #aau2013jos</p>
 				</div>
 				
 			</div>
