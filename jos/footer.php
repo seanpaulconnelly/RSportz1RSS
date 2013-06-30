a0ad7af97dfa882fa7bd573665105101b7cbf371
Sean Connelly <sean@seanconnelly.me>
2013-06-30 14:35:24 -0700

    Adjusting the rest of the schedule/results widths, closes #2

--- jos/footer.php
+++ jos/footer.php
@@ -1,10 +1,10 @@
 	<footer>
-		<div class="container">
+		<div class="container fully">
 			<div class="row">
 				<div class="span4">
 					<p class="lead rsportz">Screens Provided by RSportz <img src="../../assets/images/rsportz-logo-small.png" alt="RSportz"></p>
 				</div>
-				<div class="span4">
+				<div class="span4 offset4">
 					<p class="lead">Join in with Twitter or Instagram</p>
 				</div>
 			</div>
