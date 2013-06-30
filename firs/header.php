a59c7483429e820241b400391343a2a6bcc1da9c
Sean Connelly <sean@seanconnelly.me>
2013-06-30 13:20:25 -0700

    Removing leftover calls to js

--- firs/header.php
+++ firs/header.php
@@ -11,7 +11,5 @@
 		<script src='//html5shiv.googlecode.com/svn/trunk/html5.js'></script>
 		<![endif]-->
 		<link href="../../assets/application.css" media="screen" rel="stylesheet" type="text/css" />
-		<script src="../../assets/jquery.min.js" type="text/javascript"></script>
-		<script src="../../assets/application.min.js" type="text/javascript"></script>
 	</head>
 	<body>
