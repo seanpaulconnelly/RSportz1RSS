a29ef295912e41f44a4b9b65b1116ff8d113cfed
Sean Connelly <sean@seanconnelly.me>
2013-07-01 11:50:18 -0700

    Removing commenting around shiv, closes #7

--- jos/header.php
+++ jos/header.php
@@ -6,9 +6,7 @@
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		<meta content='RSportz is a free tool that helps you register online for leagues and tournaments, see your stats in real time, and share to social networks.' name='description'>
 		<link href='http://fonts.googleapis.com/css?family=Lato:100,400' rel='stylesheet' type='text/css'>
-		<!--[if lt IE9]>
 		<script src='//html5shiv.googlecode.com/svn/trunk/html5.js'></script>
-		<![endif]-->
 		<link href="../../assets/application.css" media="screen" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
