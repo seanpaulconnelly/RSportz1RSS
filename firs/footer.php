b8458ee4552b3e91f8a37f9c8525a3eed1534c3c
Sean Connelly <sean@seanconnelly.me>
2013-07-08 20:10:03 -0700

    More footer alterations per/social

--- firs/footer.php
+++ firs/footer.php
@@ -2,10 +2,10 @@
 		<div class="container fully">
 			<div class="row">
 				<div class="span4">
-					<p class="lead rsportz">Screens Provided by RSportz <img src="../../assets/images/rsportz-logo-small.png" alt="RSportz"></p>
+					<p class="lead rsportz">Screens Provided by RSportz <img src="../../assets/images/rsportz-logo-small.png" alt="RSportz"> @rsportz</p>
 				</div>
 				<div class="span4 offset4">
-					<p class="lead">Join in with Twitter or Instagram</p>
+					<p class="lead" style="text-align:right;">Tag your photos</p>
 				</div>
 			</div>
 		</div>
